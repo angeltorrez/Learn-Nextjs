176bab49b9f138c0ecae9056de3cd629b7540c85
Angel Torrez <angeltorrez113@gmail.com>
2025-01-25 08:39:35 -0300

    loading artificial de 3 segundos

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,8 +15,10 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
+    console.log("esperando a la carga del servidor");
     await new Promise((resolve) => setTimeout(resolve, 3000));//3 segundos de delay
     const data = await client.sql<Revenue>`SELECT * FROM revenue`;
+    console.log("la carga a finalizado luego de 3 segundos");
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
